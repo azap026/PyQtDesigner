21bbe232e38efbc9dae49cb6ea63baf17247c454
Azap026 <kiy026@yandex.ru>
2025-08-21 10:27:23 +0300

    fix: исправлен синтаксис vite.config.mjs (defineConfig, export default)

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+export default defineConfig({
   plugins: [
     react(),
     // runtimeErrorOverlay убран для production и серверной сборки
